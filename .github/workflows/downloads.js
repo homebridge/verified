5d0effe7b460334f4731abf05a1ec8a092baea75
Donavan Becker <donavan.becker@icloud.com>
2022-03-29 10:04:54 -0500

    Update downloads.js

--- .github/workflows/downloads.js
+++ .github/workflows/downloads.js
@@ -2,14 +2,6 @@
 
 var counts = require( 'npm-package-download-counts' );
  
-// Get download counts for all author packages...
-function download(fileUrl, fileName) {
-  var a = document.createElement("a");
-  a.href = fileUrl;
-  a.setAttribute("download", fileName);
-  a.click();
-}
- 
 const verified = 'https://raw.githubusercontent.com/homebridge/verified/downloads-test/verified-plugins.json';
 
 var opts = {
