7a73c01461850772c5d56e437d234f0c80c94a15
Donavan Becker <donavan.becker@icloud.com>
2022-03-28 21:53:19 -0500

    Create downloads.js

---
+++ .github/workflows/downloads.js
@@ -0,0 +1,30 @@
+#!/bin/env node
+
+var ls = require( 'npm-list-author-packages' );
+var counts = require( 'npm-package-download-counts' );
+ 
+// Get download counts for all author packages...
+var opts = ../verified-plugins.json;
+ 
+ls( opts, onList );
+ 
+function onList( error, list ) {
+    var opts;
+    if ( error ) {
+        throw error;
+    }
+    if ( !list.length ) {
+        return;
+    }
+    opts = {
+        'packages': list
+    };
+    counts( opts, onCounts );
+}
+ 
+function onCounts( error, data ) {
+    if ( error ) {
+        throw error;
+    }
+    console.dir( data );
+}
