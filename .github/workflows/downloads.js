73bea249bf22ce8d70ccc0999a02294c6380b13b
Donavan Becker <donavan.becker@icloud.com>
2022-03-29 10:10:33 -0500

    Update downloads.js

--- .github/workflows/downloads.js
+++ .github/workflows/downloads.js
@@ -1,8 +1,9 @@
 #!/bin/env node
 
 var counts = require( 'npm-package-download-counts' );
+import got from 'got';
  
-const verified = 'https://raw.githubusercontent.com/homebridge/verified/downloads-test/verified-plugins.json';
+const verified = await got.get('https://raw.githubusercontent.com/homebridge/verified/downloads-test/verified-plugins.json').json();
 
 var opts = {
     'packages': verified,
