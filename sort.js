6eafd9a58aad5af1364d597bc8a5914410d1ef33
Ben <43026681+bwp91@users.noreply.github.com>
2023-11-27 20:26:37 +0000

    add plugin icons
    Author: Donavan Becker <beckersmarthome@icloud.com>
    Date:   Mon Nov 27 13:04:33 2023 -0600

--- sort.js
+++ sort.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const verified = JSON.parse(fs.readFileSync('verified-plugins.json', 'utf8'));
 fs.writeFileSync('verified-plugins.json', JSON.stringify(verified.sort(), null, 2));
 
+const icons = JSON.parse(fs.readFileSync('plugin-icons.json', 'utf8'));
+const sorted = Object.keys(icons).sort().reduce((obj, key) => {
+  obj[key] = icons[key];
+  return obj;
+}, {});
+fs.writeFileSync('plugin-icons.json', JSON.stringify(sorted, null, 2));
+
 for (const plugin of verified.sort()) {
   console.log(`| [${plugin}](https://www.npmjs.com/package/${plugin}) |`)
 }
