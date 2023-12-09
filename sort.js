da620b2c92318f3686900c9ee312f197b76d2234
Ben <43026681+bwp91@users.noreply.github.com>
2023-12-09 17:59:03 +0000

    icon: homebridge-ambient-weather-sensors
    https://github.com/homebridge/verified/issues/617

--- sort.js
+++ sort.js
@@ -3,13 +3,28 @@ const fs = require('fs');
 const verified = JSON.parse(fs.readFileSync('verified-plugins.json', 'utf8'));
 fs.writeFileSync('verified-plugins.json', JSON.stringify(verified.sort(), null, 2) + '\n');
 
-const icons = JSON.parse(fs.readFileSync('plugin-icons.json', 'utf8'));
-const sorted = Object.keys(icons).sort().reduce((obj, key) => {
-  obj[key] = icons[key];
-  return obj;
-}, {});
-fs.writeFileSync('plugin-icons.json', JSON.stringify(sorted, null, 2) + '\n');
-
 for (const plugin of verified.sort()) {
-  console.log(`| [${plugin}](https://www.npmjs.com/package/${plugin}) |`)
+  console.log(`| [${plugin}](https://www.npmjs.com/package/${plugin}) |`);
 }
+
+const icons = JSON.parse(fs.readFileSync('plugin-icons.json', 'utf8'));
+const sorted = Object.keys(icons)
+  .filter((key) => {
+    const iconFile = icons[key];
+    if (verified.indexOf(key) === -1) {
+      console.log(` - Ignoring icon for ${key} because it is not in the verified list`);
+      return false;
+    }
+    if (!fs.existsSync(`./${iconFile}`)) {
+      console.log(` - Ignoring icon for ${key} because the icon file does not exist`);
+      return false;
+    }
+    return true;
+  })
+  .sort()
+  .reduce((obj, key) => {
+    obj[key] = icons[key];
+    return obj;
+  }, {});
+
+fs.writeFileSync('plugin-icons.json', JSON.stringify(sorted, null, 2) + '\n');
